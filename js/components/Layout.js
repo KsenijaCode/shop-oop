37c871d52baf6a7a8618fd453c69e784207195e7
KsenijaCode <Rocking@email.com>
2024-07-28 23:13:16 +0300

    Responsive

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -9,7 +9,7 @@ export class Layout {
                 <div class="row">
                     <div class="col-12">
                         <img src="#" alt="Logo">
-                        <nav>
+                        <nav class="hidden visible-sm-flex hidden-md visible-lg-grid hidden-xl visible-xxl-inline-block">
                             <a href="./">Home</a>
                             <a href="./services">Services</a>
                             <a href="./team">Team</a>
@@ -25,16 +25,19 @@ export class Layout {
         const HTML = `
             <main class="container">
                 <section class="row">
-                    <div class="col-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>
+                    <h1 class="col-12 main-title">First Turn</h1>
                 </section>
                 <section class="row">
-                    <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>
-                    <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>
+                    <div class="col-10 col-sm-8 col-md-6 col-lg-4 col-xl-2 col-xxl-1" style="background: yellow;">A</div>
+                    <div class="col-1 m-1 m-sm-3 col-md-2 m-md-4 col-lg-4 m-xl-6 col-xxl-1 m-xxl-10" style="background: orange;">B</div>
                 </section>
                 <section class="row">
-                    <div class="col-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>
-                    <div class="col-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>
-                    <div class="col-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quibusdam omnis reprehenderit eveniet officiis ea blanditiis, iure deserunt adipisci? Laborum animi expedita iusto, illo rem consequatur. Voluptatibus eveniet eius laboriosam.</div>        
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 col-xxl-12">Pirmas posukis</div>
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3">Pirmas posukis</div>
+                    <div class="col-6 col-lg-4 col-xl-3">Pirmas posukis</div>
+                    <div class="col-6 col-lg-4 col-xl-3">Pirmas posukis</div>
+                    <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-1">Pirmas posukis</div>
+                    <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2">Pirmas posukis</div>
                 </section>
             </main>`;
         return HTML;
